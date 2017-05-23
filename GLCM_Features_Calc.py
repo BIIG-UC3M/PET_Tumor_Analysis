34f8cb0e89bbb6dd11524affc86814d25804c9b6
PeterMcGor <pedro.macias.gordaliza@gmail.com>
2017-05-23 17:44:26 +0200

    Testing

--- GLCM_Features_Calc.py
+++ GLCM_Features_Calc.py
@@ -209,6 +209,14 @@ class Haralick_Features():
         return Texture_Features(self,0)
 
 if __name__ == "__main__":
+    image2_test = [np.random.randint(-1024,1024, size = (10,10,10)).astype(dtype = np.int16) for i in range(10) ]
+    #image_test = np.random.randint(-1024,1024, size = (3,3,3))
+    start = time.time()
+    for i, image_test  in enumerate(image2_test):
+        #print i
+        har = Haralick_Features(image_test, bins=16, normalization=True, save_glcm_matrices=False)
+        har.compute_features()
+    print "map",time.time() - start
 """
 import time
 image_test = np.random.randint(-1024,1024, size = (300,300,300))
@@ -221,9 +229,8 @@ start = time.time()
 har.compute_features()
 print 'process time',time.time() - start
 print har.get_average_features()
-"""
 
-"""
+
 import time
 start = time.time()
 image2_test = [np.random.randint(-1024,1024, size = (3,3,3)).astype(dtype = np.int16) for i in range(49152) ]
@@ -231,24 +238,15 @@ for image_test  in image2_test:
     har = Haralick_Features(image_test, bins=256, normalization=True, save_glcm_matrices=False)
     har.compute_features()
 print 'process time',time.time() - start
-"""
-"""
+
+
 ogg = generate_default_offsets()
 start = time.time()
 image_test = np.random.randint(-1024,1024, size = (3,3,3)).astype(dtype = np.int16)
 har = Haralick_Features(image_test, bins=256,offsets = ogg,  normalization=True, save_glcm_matrices=False)
 har.compute_features()
 print 'process time',time.time() - start
-"""
-    image2_test = [np.random.randint(-1024,1024, size = (100,200,200)).astype(dtype = np.int16) for i in range(10) ]
-    #image_test = np.random.randint(-1024,1024, size = (3,3,3))
-    start = time.time()
-    for i, image_test  in enumerate(image2_test):
-        #print i
-        har = Haralick_Features(image_test, bins=16, normalization=True, save_glcm_matrices=False)
-        har.compute_features()
-    print "map",time.time() - start
-"""
+
 start = time.time()
 for image_test  in image2_test:
     har2 = Haralick_Features(image_test, bins=256, normalization=True, save_glcm_matrices=False)
